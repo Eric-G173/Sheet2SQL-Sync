b90c45d252fcedbd35eba39a9be103d830c795f1
Eric Grana <scp00347@gmail.com>
2026-06-02 10:16:46 -0700

    Fixed another import issue

--- Sheets_Transfer.py
+++ Sheets_Transfer.py
@@ -1,5 +1,4 @@
 import psycopg2
-from google.oauth2.service_account import Credentials
 import pandas as pd
 from dotenv import load_dotenv
 import os
@@ -14,6 +13,7 @@ PASSWORD = os.getenv("DB_PASSWORD")
 
 def get_sheet():
     import gspread
+    from google.oauth2.service_account import Credentials
     SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
